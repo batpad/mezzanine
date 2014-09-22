441ff69e38f9d274248d2f8ad6d80df3f869204b
batpad <b@pad.ma>
2014-09-22 22:45:05 +0530

    Revert "Django rest framework"

--- setup.py
+++ setup.py
@@ -59,9 +59,6 @@ try:
             "requests-oauthlib >= 0.4",
             "future == 0.9.0",
             "pillow",
-            "djangorestframework",
-            "markdown",
-            "django-filter"
         ],
         entry_points="""
             [console_scripts]
